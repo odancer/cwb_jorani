41d5648ad01e6eb65727e046b66594770223c1f5
odancer <odancer@gmail.com>
2018-02-23 14:09:47 +0800

    2018-02-23_commit_v5

--- application/language/chinese/entitleddays_lang.php
+++ application/language/chinese/entitleddays_lang.php
@@ -8,21 +8,21 @@
  * @author      Ceibga Bao <info@sansin.com.tw>
  */
 
-$lang['entitleddays_contract_index_title'] = '應享有休假天數於類別中';
+$lang['entitleddays_contract_index_title'] = '應享有休假時數於類別中';
 $lang['entitleddays_contract_index_thead_tip_delete'] = '刪除';
 $lang['entitleddays_contract_index_thead_tip_copy'] = '複製';
 $lang['entitleddays_contract_index_thead_tip_edit'] = '編輯';
 $lang['entitleddays_contract_index_thead_start'] = '開始';
 $lang['entitleddays_contract_index_thead_end'] = '結束';
-$lang['entitleddays_contract_index_thead_days'] = '天數';
+$lang['entitleddays_contract_index_thead_days'] = '時數';
 $lang['entitleddays_contract_index_thead_type'] = '休假類別';
 $lang['entitleddays_contract_index_thead_description'] = '描述';
-$lang['entitleddays_contract_popup_title'] = '外加應享有天數';
+$lang['entitleddays_contract_popup_title'] = '外加應享有時數';
 $lang['entitleddays_contract_index_field_start'] = '開始日期';
 $lang['entitleddays_contract_index_field_end'] = '結束日期';
 $lang['entitleddays_contract_index_field_type'] = '休假類別';
 $lang['entitleddays_contract_index_field_description'] = '描述';
-$lang['entitleddays_contract_index_field_days'] = '天數';
+$lang['entitleddays_contract_index_field_days'] = '時數';
 $lang['entitleddays_contract_index_button_current'] = '現在';
 $lang['entitleddays_contract_index_button_add'] = '外加';
 $lang['entitleddays_contract_index_button_cancel'] = '關閉';
@@ -33,21 +33,21 @@ $lang['entitleddays_contract_confirm_delete_yes'] = '是';
 $lang['entitleddays_contract_confirm_delete_cancel'] = '取消';
 $lang['entitleddays_contract_index_field_step'] = '步驟';
 
-$lang['entitleddays_user_index_title'] = '員工應享有天數';
+$lang['entitleddays_user_index_title'] = '員工應享有時數';
 $lang['entitleddays_user_index_thead_tip_delete'] = '刪除';
 $lang['entitleddays_user_index_thead_tip_copy'] = '複製';
 $lang['entitleddays_user_index_thead_tip_edit'] = '編輯';
 $lang['entitleddays_user_index_thead_start'] = '開始';
 $lang['entitleddays_user_index_thead_end'] = '結束';
-$lang['entitleddays_user_index_thead_days'] = '天數';
+$lang['entitleddays_user_index_thead_days'] = '時數';
 $lang['entitleddays_user_index_thead_type'] = '休假類別';
 $lang['entitleddays_user_index_thead_description'] = '描述';
-$lang['entitleddays_user_popup_title'] = '外加應享有天數';
+$lang['entitleddays_user_popup_title'] = '外加應享有時數';
 $lang['entitleddays_user_index_field_start'] = '開始日期';
 $lang['entitleddays_user_index_field_end'] = '結束日期';
 $lang['entitleddays_user_index_field_type'] = '休假類別';
 $lang['entitleddays_user_index_field_description'] = '描述';
-$lang['entitleddays_user_index_field_days'] = '天數';
+$lang['entitleddays_user_index_field_days'] = '時數';
 $lang['entitleddays_user_index_button_add'] = '外加';
 $lang['entitleddays_user_index_button_cancel'] = '關閉';
 $lang['entitleddays_user_mandatory_js_msg'] = '"The field " + fieldname + " is mandatory."';
@@ -57,6 +57,6 @@ $lang['entitleddays_user_confirm_delete_yes'] = '是';
 $lang['entitleddays_user_confirm_delete_cancel'] = '取消';
 $lang['entitleddays_user_index_field_step'] = '步驟';
 
-$lang['entitleddays_counters_title_contract'] = '類別的應享有天數';
-$lang['entitleddays_counters_description_contract'] = '年休假天數';
-$lang['entitleddays_counters_title_employee'] = '員工應享有天數';
+$lang['entitleddays_counters_title_contract'] = '類別的應享有時數';
+$lang['entitleddays_counters_description_contract'] = '年休假時數';
+$lang['entitleddays_counters_title_employee'] = '員工應享有時數';
