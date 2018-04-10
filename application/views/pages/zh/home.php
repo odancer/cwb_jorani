548df9670d2d417fd7609618d173bee06018aaed
odancer <odancer@gmail.com>
2018-04-10 17:28:00 +0800

    2018-04-10_commit_v3

--- application/views/pages/zh/home.php
+++ application/views/pages/zh/home.php
@@ -1,7 +1,7 @@
 <html>
 <head>
-<?php if ($is_boss == TRUE || $is_admin == TRUE) {header('Location:requests');?><?php } ?>
 <?php if ($is_hr == TRUE) { header('Location:users'); ?><?php } ?>
+<?php if ($is_boss == TRUE || $is_admin == TRUE) {header('Location:requests');?><?php } ?>
 <?php if ($is_boss != TRUE && $is_admin != TRUE && $is_hr != TRUE) { header('Location:leaves'); ?><?php } ?>
 <meta name="keywords" content="automatic redirection">
 </head>
